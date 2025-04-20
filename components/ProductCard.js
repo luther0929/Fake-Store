4853602305cf8a2cde11107afeafb24b6e00f0cb
Luther <evangelista.luther1995@gmail.com>
2025-04-21 02:24:53 +1000

    MileStone 1 - added product screen

--- components/ProductCard.js
+++ components/ProductCard.js
@@ -4,30 +4,46 @@ export default function ProductCard({products, onPress}) {
 
     return(
         <Pressable style={styles.container} onPress={onPress}>
-            <Text>{products.title}</Text>
-            <Image style={styles.image} source={{uri: products.image}} resizeMode="contain"/>
-            <Text>{products.rating.rate}</Text>
+            <View style={styles.imageContainer}>
+                <Image style={styles.image} source={{uri: products.image}} resizeMode="contain"/>
+            </View>
+            <View style={styles.textContainer}>
+                <Text style={styles.title}>{products.title}</Text>
+                <Text>Price: ${products.price}</Text>
+                <Text>Rating: {products.rating.rate}</Text>
+            </View>
         </Pressable>
     );
 }
 
-// {"id":9,
-//     "title":"WD 2TB Elements Portable External Hard Drive - USB 3.0 ",
-//     "price":64,
-//     "description":"USB 3.0 and USB 2.0 Compatibility Fast data transfers Improve PC Performance High Capacity; Compatibility Formatted NTFS for Windows 10, Windows 8.1, Windows 7; Reformatting may be required for other operating systems; Compatibility may vary depending on user’s hardware configuration and operating system",
-//     "category":"electronics","image":"https://fakestoreapi.com/img/61IBBVJvSDL._AC_SY879_.jpg",
-//     "rating":{"rate":3.3,"count":203}}
-
 const styles = StyleSheet.create({
     container: {
-        flexDirection: 'column',
+        flexDirection: 'row',
         width: '100%',
         margin: 10,
         borderRadius: 16,
-        backgroundColor: 'white',
+        backgroundColor: '#e6d2b9',
+        justifyContent: 'space-around',
+        borderWidth: 1
+    },
+    imageContainer: {
+        margin: 12,
+        padding: 10,
+        borderWidth: 1,
+        borderRadius: 16,
+        backgroundColor: '#fff',
     },
     image: {
         width: 150,
-        height: 150
+        height: 150,
+    },
+    textContainer: {
+        paddingVertical: 10,
+        justifyContent: 'space-around',
+        width: '40%',
+        gap: 10,
+    },
+    title: {
+        flexWrap: 'wrap',
     }
 })
--- screens/ProductScreen.js
+++ screens/ProductScreen.js
@@ -6,28 +6,50 @@ export default function ProductScreen() {
     const { product } = route.params || {};
 
     return(
-        <View>
-            <Image style={styles.image} source={{uri: product.image}}/>
-            <Text>{product.title}</Text>
-            <Text>{product.price}</Text>
-            <Text>{product.description}</Text>
-            <Text>{product.category}</Text>
-            <Text>{product.rating.rate}</Text>
+        <View style={styles.container}>
+            <View style={styles.imageContainer}>
+                <Image
+                    style={styles.image}
+                    source={{uri: product.image}}
+                    resizeMode="contain"
+                />
+            </View>
+            <View style={styles.textContainer}>
+                <Text>{product.title}</Text>
+                <Text>Price ${product.price}</Text>
+                <Text>{product.description}</Text>
+                <Text>{product.category}</Text>
+                <Text>{product.rating.rate}</Text>
+            </View>
+            
         </View>
     );
 }
 
 const styles = StyleSheet.create({
+    container: {
+        flexDirection: 'column',
+        width: '100%',
+        backgroundColor: 'white',
+        alignContent: 'center',
+        borderWidth: 1,
+    },
+    imageContainer: {
+        padding: 20,
+        borderWidth: 1,
+        width: '100%',
+        alignItems: 'center',
+        borderBottomLeftRadius: 24,
+        borderBottomRightRadius: 24
+    },
     image: {
-        width: 150,
-        height: 150
+        width: 200,
+        height: 200
+    },
+    textContainer: {
+        padding: 16,
+        height: '60%', //fix this
+        backgroundColor: '#e6d2b9',
+        justifyContent: 'space-around'
     }
 })
-
-
-// {"id":9,
-//     "title":"WD 2TB Elements Portable External Hard Drive - USB 3.0 ",
-//     "price":64,
-//     "description":"USB 3.0 and USB 2.0 Compatibility Fast data transfers Improve PC Performance High Capacity; Compatibility Formatted NTFS for Windows 10, Windows 8.1, Windows 7; Reformatting may be required for other operating systems; Compatibility may vary depending on user’s hardware configuration and operating system",
-//     "category":"electronics","image":"https://fakestoreapi.com/img/61IBBVJvSDL._AC_SY879_.jpg",
-//     "rating":{"rate":3.3,"count":203}}
